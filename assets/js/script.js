7dd097e1ea410be5353beb607736104528c89b21
thisisgene <emdo2000+genegit@gmail.com>
2019-11-27 17:43:01 +0100

    Switched to soundcloud player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,101 @@
 /* ---- particles.js config ---- */
 
-let amp
-let volHistory = []
-let song
-let slider
-let buttonPlay
-let buttonStop
-let currentTimeElement
-let totalTimeElement
+// let amp
+// let volHistory = []
+// let song
+// let slider
+// let buttonPlay
+// let buttonStop
+// let currentTimeElement
+// let totalTimeElement
 
-function preload() {
-  song = loadSound(`assets/music/Iron Ore_04.mp3`)
-}
+// function preload() {
+//   song = loadSound(`assets/music/Iron Ore_04.mp3`)
+// }
 
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight)
-  // canvas.parent('graph-canvas')
-  amp = new p5.Amplitude()
+// function setup() {
+//   let canvas = createCanvas(windowWidth, windowHeight)
+//   // canvas.parent('graph-canvas')
+//   amp = new p5.Amplitude()
 
-  slider = createSlider(0, 1, 1, 0.01)
-  buttonPlay = document.getElementById('play-button')
-}
+//   slider = createSlider(0, 1, 1, 0.01)
+//   buttonPlay = document.getElementById('play-button')
+// }
 
-stopSong = () => {
-  song.stop()
-  buttonPlay.classList.remove('pause')
-}
+// stopSong = () => {
+//   song.stop()
+//   buttonPlay.classList.remove('pause')
+// }
 
-togglePlay = () => {
-  if (song.isPlaying()) {
-    song.pause()
-    buttonPlay.classList.remove('pause')
-  } else {
-    song.play()
-    buttonPlay.classList.add('pause')
-  }
-}
+// togglePlay = () => {
+//   if (song.isPlaying()) {
+//     song.pause()
+//     buttonPlay.classList.remove('pause')
+//   } else {
+//     song.play()
+//     buttonPlay.classList.add('pause')
+//   }
+// }
 
-songLoaded = id => {
-  let current = document.getElementsByClassName('current')
-  let newElement = document.getElementById(id)
-  current[0].classList.remove('current')
-  newElement.classList.add('current')
+// songLoaded = id => {
+//   let current = document.getElementsByClassName('current')
+//   let newElement = document.getElementById(id)
+//   current[0].classList.remove('current')
+//   newElement.classList.add('current')
 
-  song.play()
-  buttonPlay.classList.add('pause')
-}
+//   song.play()
+//   buttonPlay.classList.add('pause')
+// }
 
-setCurrentSong = (id, filename) => {
-  song.stop()
-  buttonPlay.classList.remove('pause')
-  console.log(filename)
-  song = ''
-  song = loadSound(`assets/music/${filename}`, songLoaded(id))
-}
+// setCurrentSong = (id, filename) => {
+//   song.stop()
+//   buttonPlay.classList.remove('pause')
+//   console.log(filename)
+//   song = ''
+//   song = loadSound(`assets/music/${filename}`, songLoaded(id))
+// }
 
-draw = () => {
-  // background('rgba(0,0,0,0.1)')
-  // // song.setVolume(slider.value())
-  // let vol = amp.getLevel()
-  // volHistory.push(vol)
+// draw = () => {
+//   // background('rgba(0,0,0,0.1)')
+//   // // song.setVolume(slider.value())
+//   // let vol = amp.getLevel()
+//   // volHistory.push(vol)
 
-  // stroke(255)
-  // // noFill()
-  // fill(10)
-  // beginShape()
-  // for (let i = 0; i < volHistory.length; i++) {
-  //   let y = map(volHistory[i], 0, 1, height, 0)
-  //   vertex(i, y)
-  // }
-  // clear()
-  // endShape()
+//   // stroke(255)
+//   // // noFill()
+//   // fill(10)
+//   // beginShape()
+//   // for (let i = 0; i < volHistory.length; i++) {
+//   //   let y = map(volHistory[i], 0, 1, height, 0)
+//   //   vertex(i, y)
+//   // }
+//   // clear()
+//   // endShape()
 
-  // if (volHistory.length > width) {
-  //   volHistory.splice(0, 1)
-  // }
+//   // if (volHistory.length > width) {
+//   //   volHistory.splice(0, 1)
+//   // }
 
-  let progress = document.getElementById('progress')
-  progress.max = song.duration()
-  progress.value = song.currentTime()
-}
+//   let progress = document.getElementById('progress')
+//   progress.max = song.duration()
+//   progress.value = song.currentTime()
+// }
 
-function pad(num) {
-  return ('0' + num).slice(-2)
-}
-function hhmmss(secs) {
-  var minutes = Math.floor(secs / 60)
-  secs = secs % 60
-  // var hours = Math.floor(minutes / 60)
-  // minutes = minutes % 60
-  return `${pad(minutes)}:${pad(secs)}`
-}
+// function pad(num) {
+//   return ('0' + num).slice(-2)
+// }
+// function hhmmss(secs) {
+//   var minutes = Math.floor(secs / 60)
+//   secs = secs % 60
+//   // var hours = Math.floor(minutes / 60)
+//   // minutes = minutes % 60
+//   return `${pad(minutes)}:${pad(secs)}`
+// }
 
 particlesJS('particles-js', {
   particles: {
     number: {
-      value: 20,
+      value: 30,
       density: {
         enable: true,
         value_area: 800
@@ -105,9 +105,9 @@ particlesJS('particles-js', {
       value: '#aaa'
     },
     shape: {
-      type: 'square',
+      type: 'circle',
       stroke: {
-        width: 10,
+        width: 0,
         color: '#fff'
       },
       polygon: {
@@ -124,13 +124,13 @@ particlesJS('particles-js', {
       random: false,
       anim: {
         enable: false,
-        speed: 1,
+        speed: 100,
         opacity_min: 0.1,
         sync: false
       }
     },
     size: {
-      value: 3,
+      value: 1,
       random: true,
       anim: {
         enable: false,
@@ -176,7 +176,7 @@ particlesJS('particles-js', {
     },
     modes: {
       grab: {
-        distance: 140,
+        distance: 240,
         line_linked: {
           opacity: 1
         }
@@ -189,7 +189,7 @@ particlesJS('particles-js', {
         speed: 3
       },
       repulse: {
-        distance: 200,
+        distance: 50,
         duration: 0.4
       },
       push: {
