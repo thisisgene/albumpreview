5885912ff1322fb839f9bb3881258772c6862799
thisisgene <emdo2000+genegit@gmail.com>
2019-12-03 14:41:31 +0100

    Added responsive Param

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,25 @@
+let keysArray = [
+  { key: 'AfHn3', value: "Ink Music's" },
+  { key: 'UdTe2', value: "Cut Surface's" },
+  { key: 'IfH3r', value: "Anna's" },
+  { key: 'JevY2', value: "Laurin's" }
+]
+
+let param
+let paramLine = document.getElementById('paramLine')
+param = window.location.search
+
+inspectParam = (param, keys) => {
+  for (let item of keys) {
+    if (param.includes(item.key)) {
+      paramLine.innerHTML = paramLine.innerHTML.replace('your', item.value)
+    }
+  }
+}
+
+inspectParam(param, keysArray)
+
 /* ---- particles.js config ---- */
-
-// let amp
-// let volHistory = []
-// let song
-// let slider
-// let buttonPlay
-// let buttonStop
-// let currentTimeElement
-// let totalTimeElement
-
-// function preload() {
-//   song = loadSound(`assets/music/Iron Ore_04.mp3`)
-// }
-
-// function setup() {
-//   let canvas = createCanvas(windowWidth, windowHeight)
-//   // canvas.parent('graph-canvas')
-//   amp = new p5.Amplitude()
-
-//   slider = createSlider(0, 1, 1, 0.01)
-//   buttonPlay = document.getElementById('play-button')
-// }
-
-// stopSong = () => {
-//   song.stop()
-//   buttonPlay.classList.remove('pause')
-// }
-
-// togglePlay = () => {
-//   if (song.isPlaying()) {
-//     song.pause()
-//     buttonPlay.classList.remove('pause')
-//   } else {
-//     song.play()
-//     buttonPlay.classList.add('pause')
-//   }
-// }
-
-// songLoaded = id => {
-//   let current = document.getElementsByClassName('current')
-//   let newElement = document.getElementById(id)
-//   current[0].classList.remove('current')
-//   newElement.classList.add('current')
-
-//   song.play()
-//   buttonPlay.classList.add('pause')
-// }
-
-// setCurrentSong = (id, filename) => {
-//   song.stop()
-//   buttonPlay.classList.remove('pause')
-//   console.log(filename)
-//   song = ''
-//   song = loadSound(`assets/music/${filename}`, songLoaded(id))
-// }
-
-// draw = () => {
-//   // background('rgba(0,0,0,0.1)')
-//   // // song.setVolume(slider.value())
-//   // let vol = amp.getLevel()
-//   // volHistory.push(vol)
-
-//   // stroke(255)
-//   // // noFill()
-//   // fill(10)
-//   // beginShape()
-//   // for (let i = 0; i < volHistory.length; i++) {
-//   //   let y = map(volHistory[i], 0, 1, height, 0)
-//   //   vertex(i, y)
-//   // }
-//   // clear()
-//   // endShape()
-
-//   // if (volHistory.length > width) {
-//   //   volHistory.splice(0, 1)
-//   // }
-
-//   let progress = document.getElementById('progress')
-//   progress.max = song.duration()
-//   progress.value = song.currentTime()
-// }
-
-// function pad(num) {
-//   return ('0' + num).slice(-2)
-// }
-// function hhmmss(secs) {
-//   var minutes = Math.floor(secs / 60)
-//   secs = secs % 60
-//   // var hours = Math.floor(minutes / 60)
-//   // minutes = minutes % 60
-//   return `${pad(minutes)}:${pad(secs)}`
-// }
 
 particlesJS('particles-js', {
   particles: {
